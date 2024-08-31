5870dcc53dc3814ba04f7b249caa90c241cd5506
jhshakil <shakil11271@gmail.com>
2024-08-31 12:39:44 +0600

    add cors crediential

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,15 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://sp-booking-client-liard.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use('/api', router);
